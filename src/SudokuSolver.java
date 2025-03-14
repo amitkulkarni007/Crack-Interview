6dd0b18dd945e1d2e2e577a4854b135ab44492df
amit <amitrkulkarni84@gmail.com>
2025-03-15 00:36:14 +0530

    Sudoku changes

--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -1,37 +1,37 @@
 public class SudokuSolver {
     public static void main(String[] args) {
-        int[][] board = {
-                {5, 3, 0, 0, 7, 0, 0, 0, 0},
-                {6, 0, 0, 1, 9, 5, 0, 0, 0},
-                {0, 9, 8, 0, 0, 0, 0, 6, 0},
-                {8, 0, 0, 0, 6, 0, 0, 0, 3},
-                {4, 0, 0, 8, 0, 3, 0, 0, 1},
-                {7, 0, 0, 0, 2, 0, 0, 0, 6},
-                {0, 6, 0, 0, 0, 0, 2, 8, 0},
-                {0, 0, 0, 4, 1, 9, 0, 0, 5},
-                {0, 0, 0, 0, 8, 0, 0, 7, 9}
-        };
+        String[][] board = {
+                {"5", "3", ".", ".", "7", ".", ".", ".", "."},
+                {"6",".",".","1","9","5",".",".","."},
+                {".","9","8",".",".",".",".","6","."},
+                {"8",".",".",".","6",".",".",".","3"},
+                {"4",".",".","8",".","3",".",".","1"},
+                {"7",".",".",".","2",".",".",".","6"},
+                {".","6",".",".",".",".","2","8","."},
+                {".",".",".","4","1","9",".",".","5"},
+                {".",".",".",".","8",".",".","7","9"}};
+
         printBoard(board);
         solveSudoku(board);
         printBoard(board);
     }
 
-    public static void solveSudoku(int[][] board) {
+    public static void solveSudoku(String[][] board) {
         solve(board);
     }
 
-    private static boolean solve(int[][] board) {
-        printBoard(board);
+    private static boolean solve(String[][] board) {
+        //printBoard(board);
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == 0) {
+                if (board[i][j].equals(".")) {
                     for (int k = 1; k <= 9; k++) {
                         if (isValid(board, i, j, k)) {
-                            board[i][j] = k;
+                            board[i][j] = ""+k;
                             if (solve(board)) {
                                 return true;
                             } else {
-                                board[i][j] = 0;
+                                board[i][j] = ".";
                             }
                         }
                     }
@@ -42,19 +42,19 @@ public class SudokuSolver {
         return true;
     }
 
-    private static boolean isValid(int[][] board, int row, int col, int num) {
+    private static boolean isValid(String[][] board, int row, int col, int num) {
         for (int i = 0; i < 9; i++) {
-            if (board[row][i] == num || board[i][col] == num) {
+            if (board[row][i].equals(""+num)  || board[i][col].equals(""+num)) {
                 return false;
             }
         }
         return true;
     }
 
-    private static void printBoard(int[][] board) {
+    private static void printBoard(String[][] board) {
         System.out.println();
-        for (int[] row : board) {
-            for (int cell : row) {
+        for (String[] row : board) {
+            for (String cell : row) {
                 System.out.print(cell + " ");
             }
             System.out.println();
