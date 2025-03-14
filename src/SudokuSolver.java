9fc5414d256a06cf717febadeff596adee9ff6ce
amit <amitrkulkarni84@gmail.com>
2025-03-15 00:07:10 +0530

    SudokuSolver via recursion

---
+++ src/SudokuSolver.java
@@ -0,0 +1,63 @@
+public class SudokuSolver {
+    public static void main(String[] args) {
+        int[][] board = {
+                {5, 3, 0, 0, 7, 0, 0, 0, 0},
+                {6, 0, 0, 1, 9, 5, 0, 0, 0},
+                {0, 9, 8, 0, 0, 0, 0, 6, 0},
+                {8, 0, 0, 0, 6, 0, 0, 0, 3},
+                {4, 0, 0, 8, 0, 3, 0, 0, 1},
+                {7, 0, 0, 0, 2, 0, 0, 0, 6},
+                {0, 6, 0, 0, 0, 0, 2, 8, 0},
+                {0, 0, 0, 4, 1, 9, 0, 0, 5},
+                {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+        printBoard(board);
+        solveSudoku(board);
+        printBoard(board);
+    }
+
+    public static void solveSudoku(int[][] board) {
+        solve(board);
+    }
+
+    private static boolean solve(int[][] board) {
+        printBoard(board);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == 0) {
+                    for (int k = 1; k <= 9; k++) {
+                        if (isValid(board, i, j, k)) {
+                            board[i][j] = k;
+                            if (solve(board)) {
+                                return true;
+                            } else {
+                                board[i][j] = 0;
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean isValid(int[][] board, int row, int col, int num) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num || board[i][col] == num) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void printBoard(int[][] board) {
+        System.out.println();
+        for (int[] row : board) {
+            for (int cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+    }
+}
